a316879cffcd264bec1af1f0ff090da6c92f4413
Justin Travis Mair <travis.mair@gmail.com>
2017-03-16 21:33:23 +0000

    Hotel View added with more complete info

--- views/hotelView.php
+++ views/hotelView.php
@@ -3,14 +3,15 @@
      <div class="col-sm-12 text-center"><h1>Information About <?php echo $hotel['name']?></h1></div>   
     <div class="col-sm-9">
         <?php 
-            echo "<p>".$hotel['location']['address']."</p>";
+            echo "<h3>".$hotel['location']['address']."</h3>";
             echo "<p>".$hotel['description']."<p>";
-            echo "Service:";
-            echo "<ul>";
-            foreach($hotel['services']as $service){
-                echo "<li>".$service."</li>";
+            echo "<h3>Rooms:</h3>";
+            echo "<table class='table table-striped'>";
+            echo "<tr><th>Type</th><th>Price</th><th>Description</th></tr>";
+            foreach($hotel['rooms']as $room){
+                echo "<tr><td>".$room['type']."</td><td>".$room['price']."</td><td>".$room['description']."</td></tr>";
             }
-            echo "</ul>";
+            echo "</table>";
         ?>
         
     </div>
@@ -23,6 +24,16 @@
           <li class="list-group-item">
             <span class="badge"><?php echo $hotel['currency']?></span>
                 Currency
+          </li>
+          <li class="list-group-item">
+                Services:
+                <?php
+                    echo "<ul>";
+                    foreach($hotel['services']as $service){
+                        echo "<li>".$service."</li>";
+                    }
+                    echo "</ul>";
+                ?>
           </li>
         </ul>
     </div>    
